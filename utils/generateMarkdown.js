667c488a3d68cb4e6c4d0d4559fdad34c03e7ca9
Maira Acevedo <m.acevedo53@yahoo.com>
2020-10-08 20:06:45 -0700

    tableofcontents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,6 +6,16 @@ function generateMarkdown(data) {
 ## description
 ${data.description}
 
+## Table of Contents (Optional)
+
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
 
 
   
